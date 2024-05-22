74eece58af0df6c26873963a8f357c07ef5fdcfe
Dang Tinh <>
2024-05-22 18:20:18 +0700

    docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AccountController } from './Account/account.controller';
 import { AccountModule } from './Account/account.module';
 
 @Module({
