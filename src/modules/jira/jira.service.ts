db950807c341ddcf3b071a242b26d954c60d5947
Dang Tinh <>
2024-06-23 00:06:07 +0700

    jira hook

--- src/modules/jira/jira.service.ts
+++ src/modules/jira/jira.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel as NestInjectModel } from '@nestjs/mongoose/dist/common/mongoose.decorators';
 import { Model } from 'mongoose';
 import { JiraLog, JiraLogDocument } from './schemas/jira_log.schema';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const _ = require('lodash');
 
 @Injectable()
